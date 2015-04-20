58d884e07e6441183922d2e43a1085b009726acc
uzyfeo <uzyfeo-x@hotmail.com>
2015-04-20 08:40:25 +0200

    Modified license and added copyright notice to source files

--- RogueCastle/FairyBossRoom.cs
+++ RogueCastle/FairyBossRoom.cs
@@ -2,9 +2,9 @@
   Rogue Legacy Enhanced
 
   This project is based on modified disassembly of Rogue Legacy's engine, with permission to do so by its creators..
-  Therefore, former creators copyright notice applies to the original disassembly and its modifications. 
+  Therefore, former creators copyright notice applies to original disassembly. 
 
-  Copyright(C) 2011-2015, Cellar Door Games Inc.
+  Disassembled source Copyright(C) 2011-2015, Cellar Door Games Inc.
   Rogue Legacy(TM) is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
 */
 
