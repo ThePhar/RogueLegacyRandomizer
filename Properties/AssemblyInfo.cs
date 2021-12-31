c142ae9630cfa2288c6e9965dafcd8e8c040f075
Zach Parks <zach@alliware.com>
2021-12-31 03:26:41 -0600

    changed project name to Rogue Legacy Randomizer

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-/*
-  Rogue Legacy Enhanced
-
-  This project is based on modified disassembly of Rogue Legacy's engine, with permission to do so by its creators.
-  Therefore, former creators copyright notice applies to original disassembly. 
-
-  Disassembled source Copyright(C) 2011-2015, Cellar Door Games Inc.
-  Rogue Legacy(TM) is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-*/
+// 
+//  RogueLegacyArchipelago - AssemblyInfo.cs
+//  Last Modified 2021-12-31
+// 
+//  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
+//  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
+// 
+//  Original Source - © 2011-2015, Cellar Door Games Inc.
+//  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
+// 
 
 using System.Diagnostics;
 using System.Reflection;
@@ -17,9 +18,9 @@ using System.Runtime.InteropServices;
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 [assembly: AssemblyCompany("Cellar Door Games, Inc.")]
 [assembly: AssemblyCopyright("Copyright © Cellar Door Games, Inc. 2015")]
-[assembly: AssemblyDescription("Rogue Legacy Archipelago")]
-[assembly: AssemblyProduct("RogueLegacyArchipelago")]
-[assembly: AssemblyTitle("Rogue Legacy Archipelago")]
+[assembly: AssemblyDescription("Rogue Legacy Randomizer")]
+[assembly: AssemblyProduct("RogueLegacyRandomizer")]
+[assembly: AssemblyTitle("Rogue Legacy Randomizer")]
 [assembly: AssemblyTrademark("Trademark Cellar Door Games, Inc. 2015")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
