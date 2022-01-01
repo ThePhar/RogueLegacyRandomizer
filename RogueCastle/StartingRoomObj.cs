5e8138bf377e4753ab20ad52ed73dcfafe03c11b
Zach Parks <zach@alliware.com>
2021-12-31 20:49:56 -0600

    added disable charon command

--- RogueCastle/StartingRoomObj.cs
+++ RogueCastle/StartingRoomObj.cs
@@ -1,13 +1,13 @@
-// 
+//
 // RogueLegacyArchipelago - StartingRoomObj.cs
 // Last Modified 2021-12-28
-// 
+//
 // This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
 // original creators. Therefore, former creators' copyright notice applies to the original disassembly.
-// 
+//
 // Original Disassembled Source - © 2011-2015, Cellar Door Games Inc.
 // Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-// 
+//
 
 using System;
 using System.Collections.Generic;
@@ -815,6 +815,9 @@ namespace RogueCastle
 
         public void TollPaid(bool chargeFee)
         {
+            if (Program.Game.ArchipelagoManager.Data.DisableCharon)
+                chargeFee = false;
+
             if (chargeFee)
             {
                 var num = Game.PlayerStats.Gold * (1f - SkillSystem.GetSkill(SkillType.PricesDown).ModifierAmount);
