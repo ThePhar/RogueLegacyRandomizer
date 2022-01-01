6b4fc201fd5ce65e5411b433bee5c036f3313817
Zach Parks <zach@alliware.com>
2022-01-01 14:30:45 -0600

    fixed getting traitors crashing the game

--- RogueCastle/Structs/SkillType.cs
+++ RogueCastle/Structs/SkillType.cs
@@ -1,13 +1,13 @@
-// 
+//
 //  RogueLegacyArchipelago - SkillType.cs
 //  Last Modified 2021-12-29
-// 
+//
 //  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
 //  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
-// 
+//
 //  Original Source - © 2011-2015, Cellar Door Games Inc.
 //  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-// 
+//
 
 namespace RogueCastle.Structs
 {
@@ -47,6 +47,7 @@ namespace RogueCastle.Structs
         NinjaUp,
         SpellSwordUp,
         SuperSecret,
+        Traitorous,
         Divider,
         AttackSpeedUp,
         InvulnerabilityAttackUp,
@@ -92,7 +93,6 @@ namespace RogueCastle.Structs
         ExplosiveExpert,
         Icicle,
         Ender,
-        Traitorous,
         Manor,
 
         // Manor Pieces
