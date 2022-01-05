b756d29565825bfd84a24c3e8614b5dbe71380c8
Zach Parks <zach@alliware.com>
2022-01-04 18:50:56 -0600

    Refactorization

--- RogueCastle/Enums/Chest.cs
+++ RogueCastle/Enums/Chest.cs
@@ -1,14 +1,3 @@
-//
-//  Rogue Legacy Randomizer - Chest.cs
-//  Last Modified 2022-01-03
-//
-//  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
-//  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
-//
-//  Original Source - © 2011-2015, Cellar Door Games Inc.
-//  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-//
-
 namespace RogueCastle.Enums
 {
     public enum Chest
--- RogueCastle/Enums/ChestCondition.cs
+++ RogueCastle/Enums/ChestCondition.cs
@@ -1,14 +1,3 @@
-//
-//  Rogue Legacy Randomizer - ChestCondition.cs
-//  Last Modified 2022-01-03
-//
-//  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
-//  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
-//
-//  Original Source - © 2011-2015, Cellar Door Games Inc.
-//  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-//
-
 namespace RogueCastle.Enums
 {
     public enum ChestCondition
@@ -16,7 +5,7 @@ namespace RogueCastle.Enums
         None,
         KillAllEnemies,
         HealthBelow15,
-        DontLook,
+        DoNotLook,
         NoJumping,
         NoSound,
         NoFloor,
