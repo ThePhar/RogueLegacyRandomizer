f5825c1334864fbcf94a2bba6c3ccae65298e704
Zach Parks <zach@alliware.com>
2022-01-06 20:12:08 -0600

    Skill cost no longer scales with player level

--- RogueCastle/SkillObj.cs
+++ RogueCastle/SkillObj.cs
@@ -1,13 +1,13 @@
-// 
+//
 //  RogueLegacyArchipelago - SkillObj.cs
 //  Last Modified 2021-12-29
-// 
+//
 //  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
 //  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
-// 
+//
 //  Original Source - © 2011-2015, Cellar Door Games Inc.
 //  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-// 
+//
 
 using DS2DEngine;
 using Microsoft.Xna.Framework;
@@ -69,7 +69,7 @@ namespace RogueCastle
 
         public int TotalCost
         {
-            get { return BaseCost + CurrentLevel * Appreciation + 10 * Game.PlayerStats.CurrentLevel; }
+            get { return BaseCost + CurrentLevel * Appreciation; }
         }
 
         public float ModifierAmount
