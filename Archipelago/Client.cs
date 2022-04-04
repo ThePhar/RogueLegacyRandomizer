f9a4a64f821bb2f54cca2507341c739637ed3370
Zach Parks <zach@alliware.com>
2022-04-04 08:31:32 -0500

    update min ap version

--- Archipelago/Client.cs
+++ Archipelago/Client.cs
@@ -1,13 +1,13 @@
-﻿// 
+﻿//
 //  Rogue Legacy Randomizer - Client.cs
 //  Last Modified 2022-04-03
-// 
+//
 //  This project is based on the modified disassembly of Rogue Legacy's engine, with permission to do so by its
 //  original creators. Therefore, the former creators' copyright notice applies to the original disassembly.
-// 
+//
 //  Original Source - © 2011-2015, Cellar Door Games Inc.
 //  Rogue Legacy™ is a trademark or registered trademark of Cellar Door Games Inc. All Rights Reserved.
-// 
+//
 
 using System;
 using System.Collections.Generic;
@@ -29,7 +29,7 @@ namespace Archipelago
     public class Client
     {
         public const int    MAXIMUM_RECONNECTION_ATTEMPTS = 3;
-        public const string MINIMUM_AP_VERSION            = "0.3.0";
+        public const string MINIMUM_AP_VERSION            = "0.3.1";
 
         private bool                            _allowReconnect;
         private DeathLinkService                _deathLinkService;
